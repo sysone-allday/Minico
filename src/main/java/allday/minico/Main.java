1d817628925fb137de037c8db5737e9a1eedab3f
onu96 <schlanger@naver.com>
2025-07-17 15:33:37 +0900

    feat: 회원가입 구현

--- src/main/java/allday/minico/Main.java
+++ src/main/java/allday/minico/Main.java
@@ -1,5 +1,6 @@
 package allday.minico;
 
+import allday.minico.utils.member.SceneManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,6 +9,21 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+
+
+    @Override
+    public void start(Stage stage) throws IOException {  // start 가 실행되면 자동으로 stage를 생성해서 매개변수로 넣어준다
+        SceneManager.init(stage); // stage
+        SceneManager.switchScene("Login");  // 최초 화면을 "Login.fxml"로 설정
+        SceneManager.getPrimaryStage().show(); // 스테이지 띄우기
+    }
+
+
+    // 수정한 것
+    // module-info 에 opens allday.minico.controller.member to javafx.fxml; 추가함
+
+
+    /*
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view/main.fxml"));
@@ -18,6 +34,9 @@ public class Main extends Application {
         stage.setMinHeight(800);
         stage.show();
     }
+    */
+
+
 
     public static void main(String[] args) {
         launch();
