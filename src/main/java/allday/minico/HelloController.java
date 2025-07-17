52ac0785afb76d3cb06c0845e42f67415a4fb531
KIM_DEAHO <102588838+DHowor1d@users.noreply.github.com>
2025-07-17 09:09:55 +0900

    first commit

---
+++ src/main/java/allday/minico/HelloController.java
@@ -0,0 +1,14 @@
+package allday.minico;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module allday.minico {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens allday.minico to javafx.fxml;
+    exports allday.minico;
+}
