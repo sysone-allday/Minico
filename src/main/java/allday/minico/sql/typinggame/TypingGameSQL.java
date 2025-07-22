802c96b407acf2f4953d3e8a8c6d3a22c4c47b8e
soyoung <so2043@naver.com>
2025-07-22 17:42:28 +0900

    style : css 클래스명 변경, 주석삭제

--- src/main/java/allday/minico/sql/typinggame/TypingGameSQL.java
+++ src/main/java/allday/minico/sql/typinggame/TypingGameSQL.java
@@ -7,29 +7,4 @@ public class TypingGameSQL {
         ORDER BY DBMS_RANDOM.VALUE
         FETCH FIRST 20 ROWS ONLY
         """;
-
-
-
-    // 랜덤으로 단어 가져오기
-//    public List<Word> selectRandomWord() {
-//        List<Word> wordList = new ArrayList<>();
-//        String sql = "SELECT * FROM word ORDER BY DBMS_RANDOM.VALUE FETCH FIRST 20 ROWS ONLY";
-//
-//        try (Connection conn = DBUtil.getConnection();
-//             PreparedStatement pstmt = conn.prepareStatement(sql);
-//             ResultSet rs = pstmt.executeQuery()) {
-//
-//            while (rs.next()) {
-//                Word word = new Word();
-//                word.setId(rs.getInt("word_id"));           // 컬럼명: id
-//                word.setText(rs.getString("word_text"));    // 컬럼명: text
-//                wordList.add(word);
-//            }
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return wordList;
-//    }
 }
